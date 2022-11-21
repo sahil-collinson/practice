24409523f09c860c658a558b3a8c1d3dc2bf279b
sahil-collinson <113420600+sahil-collinson@users.noreply.github.com>
2022-11-21 14:09:58 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
 print("hello")
+import json
+import requests
+
+
+api_url = "http://api.open-notify.org/astros.json"
+response = requests.get(api_url)
+v = response.json()
+print(v)
+json_object = json.dumps(v, indent=4)
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
